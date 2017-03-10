31ef98bd078acb47a3a733c1ecefd4c208476397
James Sral <jamessral@gmail.com>
2017-03-10 00:08:27 -0500

    Generate controllers for devise for sessions and registrations if needed; for expediency, do not yet require auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
+
+  scope module: 'api', defaults: { format: :json } do
+    namespace 'v1' do
+      post 'registrations', to: 'registrations#create'
+      post 'session', to: 'sessions#create'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
